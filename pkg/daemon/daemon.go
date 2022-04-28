e7d11fd347f828ae0c3e943706d57ca2278b8e65
Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>
2022-04-28 16:47:30 +0300

    Add '--use-systemd-service' CLI argument for config daemon

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -65,6 +65,8 @@ type Daemon struct {
 
 	platform utils.PlatformType
 
+	useSystemdService bool
+
 	client snclientset.Interface
 	// kubeClient allows interaction with Kubernetes, including the node we are running on.
 	kubeClient kubernetes.Interface
@@ -139,18 +141,20 @@ func New(
 	syncCh <-chan struct{},
 	refreshCh chan<- Message,
 	platformType utils.PlatformType,
+	useSystemdService bool,
 ) *Daemon {
 	return &Daemon{
-		name:             nodeName,
-		platform:         platformType,
-		client:           client,
-		kubeClient:       kubeClient,
+		name:              nodeName,
+		platform:          platformType,
+		useSystemdService: useSystemdService,
+		client:            client,
+		kubeClient:        kubeClient,
 		openshiftContext: openshiftContext,
-		exitCh:           exitCh,
-		stopCh:           stopCh,
-		syncCh:           syncCh,
-		refreshCh:        refreshCh,
-		nodeState:        &sriovnetworkv1.SriovNetworkNodeState{},
+		exitCh:            exitCh,
+		stopCh:            stopCh,
+		syncCh:            syncCh,
+		refreshCh:         refreshCh,
+		nodeState:         &sriovnetworkv1.SriovNetworkNodeState{},
 		drainer: &drain.Helper{
 			Client:              kubeClient,
 			Force:               true,
